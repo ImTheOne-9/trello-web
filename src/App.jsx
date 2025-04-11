4d8d908f711b467643558738872c8556189aa917
ImTheOne-9 <110813494+ImTheOne-9@users.noreply.github.com>
2025-04-11 21:54:14 +0700

    dark light mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,52 @@
-import { Button, Typography } from '@mui/material'
-import { AccessAlarm, ThreeDRotation } from '@mui/icons-material'
-import { pink } from '@mui/material/colors'
-import HomeIcon from '@mui/icons-material/Home' // default import
-
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import { useColorScheme } from '@mui/material'
+import LightModeIcon from '@mui/icons-material/LightMode'
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
+import Box from '@mui/material/Box'
 function App() {
-  return (
-    <>
-      <div>dvh1910</div>
-      <Typography variant="body2" color="text.secondary">Rbody</Typography>
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-      <AccessAlarm />
-      <ThreeDRotation />
+  const { mode, setMode } = useColorScheme()
+  if (!mode) {
+    return null
+  }
 
-      <HomeIcon />
-      <HomeIcon color="primary" />
-      <HomeIcon color="secondary" />
-      <HomeIcon color="success" />
-      <HomeIcon color="action" />
-      <HomeIcon color="disabled" />
-      <HomeIcon sx={{ color: pink[900] }} />
-    </>
+  const handleChange = (event) => {
+    setMode(event.target.value)
+  }
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
+      <Select
+        labelId="label-select-dark-light-mode"
+        id="select-dark-light-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value='light'>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <LightModeIcon fontSize='small' />
+            Light
+          </Box>
+        </MenuItem>
+        <MenuItem value='dark'>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <DarkModeOutlinedIcon fontSize='small' />
+            Dark
+          </Box>
+        </MenuItem>
+        <MenuItem value='system'>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <SettingsBrightnessIcon fontSize='small' />
+            System
+          </Box>
+        </MenuItem>
+      </Select>
+    </FormControl>
   )
 }
 
