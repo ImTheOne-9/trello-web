f19e4b5497c759502ce797d7fa9f3f91e4d9fed6
ImTheOne-9 <110813494+ImTheOne-9@users.noreply.github.com>
2025-04-12 13:35:39 +0700

    Build layout and project structure

--- eslint.config.js
+++ eslint.config.js
@@ -24,7 +24,7 @@ export default [
       //react
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'no-unused-vars': ['error', 1],
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true }
--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,10 @@
-import InputLabel from '@mui/material/InputLabel'
-import MenuItem from '@mui/material/MenuItem'
-import FormControl from '@mui/material/FormControl'
-import Select from '@mui/material/Select'
-import { useColorScheme } from '@mui/material'
-import LightModeIcon from '@mui/icons-material/LightMode'
-import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
-import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
-import Box from '@mui/material/Box'
+import React from 'react'
+import Board from './pages/Boards/_id'
 function App() {
-  const { mode, setMode } = useColorScheme()
-  if (!mode) {
-    return null
-  }
-
-  const handleChange = (event) => {
-    setMode(event.target.value)
-  }
-
   return (
-    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-      <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
-      <Select
-        labelId="label-select-dark-light-mode"
-        id="select-dark-light-mode"
-        value={mode}
-        label="Mode"
-        onChange={handleChange}
-      >
-        <MenuItem value='light'>
-          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-            <LightModeIcon fontSize='small' />
-            Light
-          </Box>
-        </MenuItem>
-        <MenuItem value='dark'>
-          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-            <DarkModeOutlinedIcon fontSize='small' />
-            Dark
-          </Box>
-        </MenuItem>
-        <MenuItem value='system'>
-          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-            <SettingsBrightnessIcon fontSize='small' />
-            System
-          </Box>
-        </MenuItem>
-      </Select>
-    </FormControl>
+    <>
+      <Board />
+    </>
   )
 }
-
 export default App
