6f4eaa9f38d2eb8716d1c694a0e7b45ed3f0860e
ImTheOne-9 <110813494+ImTheOne-9@users.noreply.github.com>
2025-05-26 18:38:09 +0700

    Handle Add New Column & Card

--- src/apis/index.js
+++ src/apis/index.js
@@ -4,3 +4,13 @@ export const fetchBoardDetailsAPI = async (boardId) => {
   const response = await axiosClient.get(`/v1/boards/${boardId}`)
   return response.data
 }
+
+export const createNewColumnAPIs = async (columnData) => {
+  const response = await axiosClient.post('/v1/columns', columnData)
+  return response.data
+}
+
+export const createNewCardAPIs = async (cardData) => {
+  const response = await axiosClient.post('/v1/cards', cardData)
+  return response.data
+}
